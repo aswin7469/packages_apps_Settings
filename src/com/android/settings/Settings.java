f70277a8a9887ca2c65e066d6ab5fc86af6f3432
aswin7469 <aswinas@pixysos.com>
2023-01-13 21:27:04 +0530

    Settings: Move Theming to settings homepage
    * cherry-picked from https://github.com/PixysOS/packages_apps_Settings@8eab284f12401acbd4f8b64d8a17775e41f7163e
    
    Change-Id: Iee80b3cb415313ed25d3da66002cc6a9ed1cc5fa
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -491,4 +491,9 @@ public class Settings extends SettingsActivity {
      * Activity for PreviouslyConnectedDeviceDashboardFragment
      */
     public static class PreviouslyConnectedDevicesActivity extends SettingsActivity { /* empty */ }
+
+   /**
+     * Activity for Pixys Themes
+     */
+    public static class StartThemesActivity extends SettingsActivity {}
 }
--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -22,6 +22,7 @@ import com.android.settings.MainClear;
 import com.android.settings.MainClearConfirm;
 import com.android.settings.Settings;
 import com.pixys.settings.laboratory.LabSettings;
+import com.pixys.settings.theme.ThemeSettings;
 import com.android.settings.TestingSettings;
 import com.android.settings.TrustedCredentialsSettings;
 import com.android.settings.accessibility.AccessibilityDetailsSettingsFragment;
@@ -377,7 +378,8 @@ public class SettingsGateway {
             UserAspectRatioDetails.class.getName(),
             ScreenTimeoutSettings.class.getName(),
             LabSettings.class.getName(),
-            WifiTetherClientManager.class.getName()
+            WifiTetherClientManager.class.getName(),
+            ThemeSettings.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
